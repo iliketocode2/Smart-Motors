974db514a516ab9721968e1451c847643221bfba
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2025-06-26 10:29:40 -0400

    better motor control

--- final_websocket/code_to_run_on_summer25_for_new_hardware.py
+++ final_websocket/code_to_run_on_summer25_for_new_hardware.py
@@ -4,12 +4,16 @@ import json
 async def myCallback(message):
     value = json.loads(message.decode())
 
-def fred(message):
+def fred(message):    
+    await x.motor_speed(1, 50)
     payload = message['payload']
     payload_dict = json.loads(payload)
     topic = payload_dict['topic']
     value = payload_dict['value']
-    if (topic == "/controller/data"):
-        await x.right_angle(angle = value)
+    
+    if (topic == "/controller/data") and (value != "heartbeat"):
+        final_pos = int((value / 180) * 270)
+        print("Final Pos: ", final_pos)
+        await x.right_abs_pos(final_pos)
     
 myChannel.callback = fred
